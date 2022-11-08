96dc33beded2c89f7fb527543e8537feb15b1326
CodeDrummer <code.drummer87@gmail.com>
2022-11-08 19:29:41 +0600

    Positions table created

---
+++ Database/5.create_positions_table/SQL/create_positions_table.sql
@@ -0,0 +1,20 @@
+﻿IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE ID = OBJECT_ID(N'tchDb'))
+USE tchDb;
+
+GO
+
+IF OBJECT_ID('Positions', 'u') IS NULL
+CREATE TABLE Positions
+(
+	Id INT IDENTITY(1, 1) PRIMARY KEY,
+	FullName NVARCHAR(30),
+	Abbreviate NVARCHAR(20)
+)
+
+GO
+
+INSERT INTO Positions
+VALUES
+	('Помощник машиниста', 'ТЧПМ'),
+	('Машинист', 'ТЧМ'),
+	('Машинист-инструктор', 'ТЧМИ')
