2dc36547a94ec880308307afef97fc04fa008042
CodeDrummer <code.drummer87@gmail.com>
2022-12-12 23:51:19 +0600

    Inserted data about big stations in the TrainModeTrafficLights table

---
+++ Database/18.insert_into_trainModeTrafficLights_table/SQL_ROLLBACK/rollback_changes_in_trainModeTrafficLights_table.sql
@@ -0,0 +1,7 @@
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE ID = OBJECT_ID(N'tchDb'))
+USE tchDb;
+
+GO
+
+IF OBJECT_ID('TrainModeTrafficLights', 'u') IS NOT NULL
+DELETE FROM TrainModeTrafficLights WHERE Id>148
