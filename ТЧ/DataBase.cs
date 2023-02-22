c6d88bfb316bebdf4b4e2026faad6dd2d63f6a1f
CodeDrummer <code.drummer87@gmail.com>
2023-02-22 15:39:32 +0600

    Release x86 version (laptop)

---
+++ ТЧ/DataBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace TCH_desktop
+{
+    internal static class DataBase
+    {
+        static SqliteConnection sqliteConnection = new("Data Source=tch.db");
+
+        public static void OpenConnection()
+        {
+            if (sqliteConnection.State == System.Data.ConnectionState.Closed)
+                sqliteConnection.Open();
+        }
+
+        public static void CloseConnection()
+        {
+            if (sqliteConnection.State == System.Data.ConnectionState.Open)
+                sqliteConnection.Close();
+        }
+
+        public static SqliteConnection GetConnection() => sqliteConnection;
+    }
+}
