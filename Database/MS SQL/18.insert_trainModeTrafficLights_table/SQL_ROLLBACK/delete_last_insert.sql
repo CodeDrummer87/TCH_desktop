052a0899e7d8f61d8bc32baae5c2bb22d66f318e
CodeDrummer <code.drummer87@gmail.com>
2023-02-25 20:19:05 +0600

    All signals of the West Siberian railway are included in the database

---
+++ Database/MS SQL/18.insert_trainModeTrafficLights_table/SQL_ROLLBACK/delete_last_insert.sql
@@ -0,0 +1,7 @@
+﻿IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE ID = OBJECT_ID(N'tchDb'))
+USE tchDb;
+
+GO
+
+IF OBJECT_ID(N'TrainModeTrafficLights', 'u') IS NULL
+DELETE FROM TrainModeTrafficLights WHERE Id>730
